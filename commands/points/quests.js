747b789d2c363b6699469f2756da11d5eeec3864
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2020-11-18 18:06:39 +0100

    pog

--- commands/points/quests.js
+++ commands/points/quests.js
@@ -41,6 +41,40 @@ return
         message.reply(Embed);
     db.add(`points_${message.author.id}`, `1000`)
     db.delete(`${message.author.id}_quests`)
+    
+          let mins = Math.ceil(1);
+    let maxs = Math.floor(4); 
+    const index = Math.floor(
+				Math.floor(Math.random() * (maxs - mins) + mins)
+            );
+            
+
+console.log(index)
+
+if(index == '1'){
+        db.set(`${message.author.id}_quests`, `rob`) 
+             const Embed = new Discord.MessageEmbed() // talking
+        .setTitle('Rob Quest')
+        .setDescription(`**Rob a guy to finish this quest.**`) 
+        .setColor('#FF0000')
+        message.reply(Embed);
+}else if(index == '2'){
+        db.set(`${message.author.id}_quests`, `gamble`) 
+             const Embed = new Discord.MessageEmbed() // talking
+        .setTitle('Points!')
+        .setDescription(`**gamble 1k points at once to finish this quest**`) 
+        .setColor('#FF0000')
+        message.reply(Embed);
+}else if(index == '3'){
+        db.set(`${message.author.id}_quests`, `share`) 
+             const Embed = new Discord.MessageEmbed() // talking
+        .setTitle('Points!')
+        .setDescription(`**Share 1k points at once to complete this quest**`) 
+        .setColor('#FF0000')
+        message.reply(Embed);
+}
+    
+    
     return
 }else {
 
